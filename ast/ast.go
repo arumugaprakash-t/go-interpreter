3e1197dacb1807350e7ebe68b1668ad07a117525
Arumugaprakash <tarumugaprakash@gmail.com>
2024-10-06 19:53:35 +0530

    added parser package and let statement parser

---
+++ ast/ast.go
@@ -0,0 +1,53 @@
+package ast
+
+import (
+	"go-interpreter/token"
+)
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+func (l *LetStatement) statementNode() {}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (l *Identifier) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+func (l *Identifier) expressionNode() {}
---
+++ parser/parser_test.go
@@ -0,0 +1,112 @@
+package parser
+
+import (
+	"testing"
+
+	"go-interpreter/ast"
+	"go-interpreter/lexer"
+)
+
+func Test_LetStatements(t *testing.T) {
+	t.Run("should parse the let statement successfully", func(t *testing.T) {
+		input := `
+let x = 5;
+let y = 10;
+let foobar = 121212;
+`
+		l := lexer.New(input)
+		p := New(l)
+
+		program := p.ParseProgram()
+		checkParseErrors(t, p)
+		if program == nil {
+			t.Fatalf("ParseProgram() returned nil")
+		}
+		if len(program.Statements) != 3 {
+			t.Fatalf("program.Statements doesn't contain 3 statements")
+		}
+
+		tests := []struct {
+			expressionIdentifier string
+		}{
+			{"x"},
+			{"y"},
+			{"foobar"},
+		}
+
+		for i, tt := range tests {
+			stmt := program.Statements[i]
+			if !testLetStatement(t, stmt, tt.expressionIdentifier) {
+				return
+			}
+		}
+	})
+
+	t.Run("should fail to parse the let statement", func(t *testing.T) {
+		input := `
+let x 5;
+let = 10;
+let 123;`
+
+		l := lexer.New(input)
+		p := New(l)
+
+		program := p.ParseProgram()
+		checkParseErrors(t, p)
+		if program == nil {
+			t.Fatalf("ParseProgram() returned nil")
+		}
+		if len(program.Statements) != 3 {
+			t.Fatalf("program.Statements doesn't contain 3 statements")
+		}
+
+		tests := []struct {
+			expressionIdentifier string
+		}{
+			{"x"},
+			{"y"},
+			{"foobar"},
+		}
+
+		for i, tt := range tests {
+			stmt := program.Statements[i]
+			if !testLetStatement(t, stmt, tt.expressionIdentifier) {
+				return
+			}
+		}
+	})
+
+}
+
+func testLetStatement(t *testing.T, s ast.Statement, name string) bool {
+	if s.TokenLiteral() != "let" {
+		t.Errorf("s.TokenLiteral is not let, got=%q", s.TokenLiteral())
+		return false
+	}
+	letStmt, ok := s.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("assertion to LetStatement failed, got=%T", s)
+		return false
+	}
+	if letStmt.Name.Value != name {
+		t.Errorf("letStmt.Name.Value not '%s'. got=%s", name, letStmt.Name.Value)
+		return false
+	}
+	if letStmt.Name.TokenLiteral() != name {
+		t.Errorf("s.Name not '%s'. got=%s", name, letStmt.Name)
+		return false
+	}
+	return true
+}
+
+func checkParseErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
