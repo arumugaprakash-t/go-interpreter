4f0204c101da1a23490f2cb3bc0e935cec19662a
Arumugaprakash <tarumugaprakash@gmail.com>
2024-10-13 12:52:55 +0530

    added expression statement

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,14 @@
 package ast
 
 import (
+	"bytes"
+
 	"go-interpreter/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,6 +32,15 @@ func (p *Program) TokenLiteral() string {
 	return ""
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, st := range p.Statements {
+		out.WriteString(st.String())
+	}
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token
 	Name  *Identifier
@@ -41,6 +53,21 @@ func (l *LetStatement) TokenLiteral() string {
 
 func (l *LetStatement) statementNode() {}
 
+func (l *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(l.TokenLiteral())
+	out.WriteString(l.Name.Value)
+
+	if l.Value != nil {
+		out.WriteString(" = ")
+		out.WriteString(l.Value.String())
+	}
+
+	out.WriteString(";")
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token
 	Value string
@@ -52,6 +79,10 @@ func (l *Identifier) TokenLiteral() string {
 
 func (l *Identifier) expressionNode() {}
 
+func (l *Identifier) String() string {
+	return l.Value
+}
+
 type ReturnStatement struct {
 	Token       token.Token
 	ReturnValue Expression
@@ -61,4 +92,35 @@ func (r *ReturnStatement) statementNode() {}
 
 func (r *ReturnStatement) TokenLiteral() string {
 	return r.Token.Literal
+}
+
+func (r *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(r.TokenLiteral() + " ")
+
+	if r.ReturnValue != nil {
+		out.WriteString(r.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (e *ExpressionStatement) statementNode() {}
+func (e *ExpressionStatement) TokenLiteral() string {
+	return e.Token.Literal
+}
+
+func (e *ExpressionStatement) String() string {
+	if e.Expression != nil {
+		return e.Expression.String()
+	}
+
+	return ""
 }
